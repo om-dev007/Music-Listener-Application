0d5002a4d400bbada462abf673c82e93269493d9
om-dev007 <officialomnarayan@gmail.com>
2026-03-03 09:11:02 +0530

    album routes created

--- backend/src/routes/music.routes.js
+++ backend/src/routes/music.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createMusic } from "../controllers/music.controller.js";
+import { createAlbum, createMusic } from "../controllers/music.controller.js";
 import { upload } from "../config/multer.config.js";
 
 const musicRouter = Router()
 
 musicRouter.post("/upload", upload.single("music"), createMusic)
+musicRouter.post("/album", createAlbum)
 
 export default musicRouter
